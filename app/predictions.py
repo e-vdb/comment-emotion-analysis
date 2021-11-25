10a245f8a24b7f33305162e0276b46528e1906d0
emelinevanderbeken <emelinevanderbeken@gmail.com>
2021-11-25 16:04:07 +0100

    Defined make_prediction function

--- app/predictions.py
+++ app/predictions.py
@@ -3,3 +3,9 @@ import pickle
 def load_pickle(filename):
   """Load  from disk."""
   return pickle.load(open(filename, 'rb'))
+
+
+def make_prediction(model, encoder, entry):
+  y_pred = model.predict([entry])
+  output = encoder.inverse_transform(y_pred.reshape(-1, 1))[0][0]
+  return  output
