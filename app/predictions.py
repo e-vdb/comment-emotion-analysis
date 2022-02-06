f8f5a5ad7c8eb1e55beb5d61d63fa61c66519dfd
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-02-06 18:29:53 +0100

    Added documentation

--- app/predictions.py
+++ app/predictions.py
@@ -1,11 +1,22 @@
 import pickle
 
-def load_pickle(filename):
-  """Load  from disk."""
+
+def load_pickle(filename: str):
+  """
+  Load a file  from disk.
+
+  Parameters
+  ----------
+  filename: str
+    Name of the file that is loaded.
+  Returns
+  -------
+
+  """
   return pickle.load(open(filename, 'rb'))
 
 
 def make_prediction(model, encoder, entry):
   y_pred = model.predict([entry])
   output = encoder.inverse_transform(y_pred.reshape(-1, 1))[0][0]
-  return  output
+  return output
