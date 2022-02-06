d446e9e70cda63e203f34c87cc40c9bb5f9071e3
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-02-06 19:04:52 +0100

    Defined class to handle the app

--- app/frontend.py
+++ app/frontend.py
@@ -1,22 +1,48 @@
 import streamlit as st
 from predictions import load_pickle, make_prediction
+import sys
+from streamlit import cli as stcli
 
-ML_model = load_pickle('app/finalized_model.sav')
-encoder = load_pickle('app/encoder.pickle')
+st.set_page_config(layout="wide")
 
-# Set the app title
-st.title("Emotion Analysis App")
-st.write(
-    "A simple machine learning app to predict the emotion from a user's comment."
-)
-# Declare a form to receive a comment
-form = st.form(key="my_form")
-comment = form.text_input(label="Enter a comment describing your mood")
-submit = form.form_submit_button(label="Make Prediction")
-if submit:
-    output = make_prediction(ML_model, encoder, comment)
-    # Display results of the NLP task
-    st.header("Results")
-    st.write(output)
-    filename = 'app/pictures/' + output + '.jpg'
-    st.image(filename)
+
+class App:
+    def __init__(self):
+        self.ML_model = load_pickle('finalized_model.sav')
+        self.encoder = load_pickle('encoder.pickle')
+
+    def show(self):
+        # Set the app title
+        st.title("Emotion Analysis App")
+        st.write(
+            "A simple machine learning app to predict the emotion from a user's comment."
+        )
+        self.get_comment()
+
+    def get_comment(self):
+        # Declare a form to receive a comment
+        with st.form('test'):
+            self.comment = st.text_input(label="How do you feel today?")
+            if st.form_submit_button('Make prediction'):
+                self.display_prediction()
+
+    def display_prediction(self):
+        output = make_prediction(self.ML_model, self.encoder, self.comment)
+        # Display results of the NLP task
+        st.header("Results")
+        st.write(output)
+        filename = 'pictures/' + output + '.jpg'
+        st.image(filename, use_column_width=False)
+
+
+def main():
+    front = App()
+    front.show()
+
+
+if __name__ == "__main__":
+    if st._is_running_with_streamlit:
+        main()
+    else:
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(stcli.main())
