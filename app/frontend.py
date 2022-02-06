2f116e32a71b29dbb2995263898a64f9db5e016a
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-02-06 19:31:46 +0100

    Added muliple choices in sidebar

--- app/frontend.py
+++ app/frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 from predictions import load_pickle, make_prediction
 import sys
 from streamlit import cli as stcli
-
+from constants import CHOICES
 st.set_page_config(layout="wide")
 
 
@@ -10,17 +10,38 @@ class App:
     def __init__(self):
         self.ML_model = load_pickle('finalized_model.sav')
         self.encoder = load_pickle('encoder.pickle')
+        self.comment = ""
+
+    def choose(self) -> None:
+        """Display the different possible actions."""
+        self.choice = st.sidebar.selectbox(
+            label="What would you like to do?",
+            options=CHOICES,
+            help="You need to select what you'd like the application to do.",
+        )
 
     def show(self):
         # Set the app title
-        st.title("Emotion Analysis App")
+        st.title("Streamlit demo ")
         st.write(
-            "A simple machine learning app to predict the emotion from a user's comment."
+            "A few examples of streamlit web applications "
         )
-        self.get_comment()
+        self.choose()
+        if self.choice is not None:
+            callables = [self.get_comment]
 
-    def get_comment(self):
+            args = [[]]
+            possible_choices = CHOICES[1:]
+            map_actions_to_callables = dict(zip(possible_choices, callables))
+
+            print(self.choice)
+            print(map_actions_to_callables)
+            # Calls the chosen function with relevant arguments
+            map_actions_to_callables[self.choice](*args[0])
+
+    def get_comment(self, *args):
         # Declare a form to receive a comment
+        st.write("A machine learning application to predict the emotion from a user's comment.")
         with st.form('test'):
             self.comment = st.text_input(label="How do you feel today?")
             if st.form_submit_button('Make prediction'):
