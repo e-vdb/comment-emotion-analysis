183429bd6378a15184f07d953e754d7c2516db8e
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-02-09 18:16:43 +0100

    Updated EMOTION_ANALYSIS

--- app/constants.py
+++ app/constants.py
@@ -3,8 +3,21 @@ CHOICES = (None,
            'Emotion analysis')
 
 EMOTION_ANALYSIS = """
+                   ## Emotion Analysis
+                   ### Description
                    This application uses a machine learning algorithm in order to guess the emotion of 
                    the user from its comment.
-                   
-                   The dataset used to train the model can be found in https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp
+                   ### Dataset
+                   Emotion dataset for NLP multi-class classification with six classes:
+                   - joy
+                   -  love
+                   - sadness
+                   - fear
+                   - surprise
+                   - anger
+
+                   This dataset is available in Kaggle https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp
+                   ### Machine learning model
+                   - Natural Language Processing
+                   - Model:  Linear support vector machine
                    """
