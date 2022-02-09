83f05291b224234b58538834eb26dc9d6d00ed01
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-02-09 17:58:17 +0100

    Added constant to describe the emotion analysis application

--- app/constants.py
+++ app/constants.py
@@ -1,1 +1,10 @@
-CHOICES = (None, 'Emotion analysis')
+CHOICES = (None,
+           'Display information',
+           'Emotion analysis')
+
+EMOTION_ANALYSIS = """
+                   This application uses a machine learning algorithm in order to guess the emotion of 
+                   the user from its comment.
+                   
+                   The dataset used to train the model can be found in https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp
+                   """
