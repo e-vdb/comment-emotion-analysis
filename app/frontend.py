ae5a9a8610d422ec4003561a7fc8359965783ed3
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-02-19 19:43:02 +0100

    Defined collect_contribution to receive user true feeling and added output as session state

--- app/frontend.py
+++ app/frontend.py
@@ -8,9 +8,10 @@ st.set_page_config(layout="wide")
 
 class App:
     def __init__(self):
-        self.ML_model = load_pickle('app/finalized_model.sav')
-        self.encoder = load_pickle('app/encoder.pickle')
+        self.ML_model = load_pickle('finalized_model.sav')
+        self.encoder = load_pickle('encoder.pickle')
         self.comment = ""
+
 
     def choose(self) -> None:
         """Display the different possible actions."""
@@ -38,26 +39,44 @@ class App:
             possible_choices = CHOICES[1:]
             map_actions_to_callables = dict(zip(possible_choices, callables))
 
-            print(self.choice)
-            print(map_actions_to_callables)
             # Calls the chosen function with relevant arguments
             map_actions_to_callables[self.choice](*args[0])
 
     def get_comment(self, *args):
         # Declare a form to receive a comment
         st.write("A machine learning application that predicts the emotion from a user's comment.")
-        with st.form('test'):
+
+        with st.form('test', clear_on_submit=True):
             self.comment = st.text_input(label="💬 How do you feel today?")
-            if st.form_submit_button(' 👉 Make prediction'):
+            submit = st.form_submit_button(' 👉 Make prediction')
+        if submit:
+            if self.comment == "":
+                st.warning("Please enter a comment before submit")
+            else:
                 self.display_prediction()
+        self.collect_contribution()
 
     def display_prediction(self):
-        output = make_prediction(self.ML_model, self.encoder, self.comment)
+        self.output = make_prediction(self.ML_model, self.encoder, self.comment)
         # Display results of the NLP task
         st.header("Result")
-        st.write(output)
-        filename = 'app/pictures/' + output + '.jpg'
+        st.write(self.output)
+        filename = 'pictures/' + self.output + '.jpg'
         st.image(filename, use_column_width=False)
+        st.session_state.output = self.output
+
+    def collect_contribution(self):
+        if 'output' in st.session_state:
+            st.write("Contribute to improve the model's performances")
+            true_feeling = st.selectbox(label="Which feeling was the most relevant?",
+                             options=['sadness', 'joy', 'love', 'anger', 'surprised', 'fear'])
+            row = {'Comment': self.comment, 'Prediction': st.session_state.output, 'True': true_feeling}
+            button = st.button('submit')
+            if button:
+                st.write(row)
+                del st.session_state['output']
+                st.success("Thanks for your contribution!")
+
 
 
 def main():
